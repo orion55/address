45227e5c4e75024e15c3cc7246b77b83ebe6e631
Orion55 <grol55@mail.ru>
2019-04-28 22:29:35 +0500

    first commit

---
+++ src/components/ListAddress.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div v-if="loading">
+            loading...
+        </div>
+        <div v-else>
+            <ul>
+                <li v-for="item in adds">
+                    <h1>
+                        {{item.city}}
+                    </h1>
+                    <p>
+                        {{item.street}}<br/>
+                        {{item.size}}
+                    </p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+  import { mapState } from 'vuex'
+
+  export default {
+    name: 'ListAddress',
+    mounted () {
+      this.$store.dispatch('loadData');
+    },
+    computed: mapState(['adds', 'loading']),
+  }
+
+</script>
+
+<style scoped>
+
+</style>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,57 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
-Vue.use(Vuex)
+Vue.use(Vuex, axios)
+
+const URL = 'https://randomuser.me/api/?results=20&nat=us'
 
 export default new Vuex.Store({
   state: {
-
-  },
-  mutations: {
-
+    adds: [],
+    loading: true,
   },
   actions: {
-
-  }
+    loadData ({commit}) {
+      axios.get(URL).then((response) => {
+        // console.log(response.data.results, this)
+        commit('updateAddress', response.data.results)
+        commit('changeLoadingState', false)
+      })
+    },
+  },
+  mutations: {
+    updateAddress (state, address) {
+      address.forEach((item) => {
+          const location = item.location
+          const obj = {
+            check: false,
+            city: location.city,
+            street: location.street,
+            coordinates: Object.assign(location.coordinates),
+            sides: [
+              {
+                name: 'A',
+                status: false,
+                img: '',
+                lighting: false,
+              },
+              {
+                name: 'B',
+                status: false,
+                img: '',
+                lighting: false,
+              },
+            ],
+            code: item.phone,
+            size: location.state,
+          }
+          state.adds.push(obj)
+        },
+      )
+    },
+    changeLoadingState (state, loading) {
+      state.loading = loading
+    },
+  },
 })
