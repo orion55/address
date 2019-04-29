1a177af788939f50eceeda8831bd60d3cd3127c7
orion55 <grol55@mail.ru>
2019-04-29 16:50:59 +0500

    вёрстка #02

--- src/components/ListAddress.vue
+++ src/components/ListAddress.vue
@@ -5,24 +5,27 @@
         </div>
         <div v-else class="list-adds__box">
             <div class="list-adds__title">Адресная программа</div>
-            <div class="list-adds__header">
-                <div class="list-adds__col list-adds__col--1"></div>
-                <div class="list-adds__col list-adds__col--2">Город</div>
-                <div class="list-adds__col list-adds__col--3">Адрес, направление</div>
-                <div class="list-adds__col list-adds__col--4">Сторона</div>
-            </div>
-            <div class="list-adds__body">
-                <ul>
-                    <li v-for="item in adds">
-                        <h1>
-                            {{item.city}}
-                        </h1>
-                        <p>
-                            {{item.street}}<br/>
-                            {{item.size}}
-                        </p>
-                    </li>
-                </ul>
+            <div class="list-adds__wrap">
+                <div class="list-adds__header">
+                    <div class="list-adds__col list-adds__col--1"></div>
+                    <div class="list-adds__col list-adds__col--2">Город</div>
+                    <div class="list-adds__col list-adds__col--3">Адрес, направление</div>
+                    <div class="list-adds__col list-adds__col--4">Сторона</div>
+                </div>
+                <div class="list-adds__body">
+                    <ul>
+                        <li v-for="item in adds">
+                            <h1>
+                                {{item.city}}
+                            </h1>
+                            <p>
+                                {{item.street}}<br/>
+                                {{item.size}}
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+                <FooterAddress></FooterAddress>
             </div>
         </div>
 
@@ -31,9 +34,13 @@
 
 <script>
   import { mapState } from 'vuex'
+  import FooterAddress from './FooterAddress'
 
   export default {
     name: 'ListAddress',
+    components: {
+      FooterAddress,
+    },
     mounted () {
       this.$store.dispatch('loadData')
     },
@@ -45,7 +52,7 @@
 <style lang="scss">
     .list-adds {
         height: 100%;
-        padding-right: 4px;
+        /*padding-right: 4px;*/
     }
 
     .list-adds__title {
@@ -59,8 +66,8 @@
 
     .list-adds__body {
         overflow: auto;
-        height: 87%;
-        padding: 10px;
+        margin: 10px 4px 10px 10px;
+        flex: 1;
     }
 
     .list-adds__box {
@@ -101,5 +108,16 @@
         box-shadow: 0 2px 4px rgba(217, 220, 227, 0.5);
         padding-top: 18px;
         padding-bottom: 18px;
+        height: 60px;
+    }
+
+    .list-adds__wrap {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: stretch;
+        align-content: stretch;
+        min-height: 90%;
+        height: calc(91% + 2px);
     }
 </style>
